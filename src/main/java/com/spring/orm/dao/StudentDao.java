b2be58b8fef5bbfa5259137fb89d5638f6adb38c
Nitya <22bch036@nith.ac.in>
2025-05-02 16:43:00 +0530

    successfully completed crud operation- inserting, accessing, deleting, updating data

--- src/main/java/com/spring/orm/dao/StudentDao.java
+++ src/main/java/com/spring/orm/dao/StudentDao.java
@@ -1,5 +1,7 @@
 package com.spring.orm.dao;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.orm.hibernate5.HibernateTemplate;
@@ -17,6 +19,31 @@ public class StudentDao {
 		Integer i= (Integer)this.hibernateTemplate.save(student);
 		
 		return 1;		
+	}
+	
+	//get single data(object)
+	public Student getStudent(int studentId) {
+		Student student= this.hibernateTemplate.get(Student.class, studentId);
+		return student;
+	}
+	
+	//get all students(all rows)
+	public List<Student> getAllStudents(){
+		List<Student> students = this.hibernateTemplate.loadAll(Student.class);
+		return students;
+	}
+	
+	//deleting the rows
+	@Transactional
+	public void deleteStudent(int studentId) {
+		Student student= this.hibernateTemplate.get(Student.class, studentId);
+		this.hibernateTemplate.delete(student);
+	}
+	
+	//updating data
+	@Transactional
+	public void updateStudent(Student student) {
+		this.hibernateTemplate.update(student);
 	}
 
 	public HibernateTemplate getHibernateTemplate() {
